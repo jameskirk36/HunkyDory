430c37c75bf2a3662fa02dd57a3f5f56a2f67d6a
James Kirk <james.kirk84@gmail.com>
2014-02-18 18:11:22 +0000

    Fixed first test

--- specs/homepage.rb
+++ specs/homepage.rb
@@ -4,7 +4,7 @@ describe "Homepage" do
 	it "should hit the homepage and title should say hunky dory chocolates" do
 		driver = Selenium::WebDriver.for :firefox
 		driver.navigate.to "http://localhost:4567"
-		driver.find_element(:tag_name => "title").text.should eq( "hunk dory chocolates")
+		driver.title.should eq("hunky dory")
 		driver.quit
 	end 
 end
